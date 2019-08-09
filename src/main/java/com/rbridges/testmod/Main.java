c3d32ec8b78dcce1bfd3f828c2ac1665e7313c38
Riley Bridges <rlybrdgs@gmail.com>
2019-08-08 20:49:28 -0500

    Finished Reference class

--- src/main/java/com/rbridges/testmod/Main.java
+++ src/main/java/com/rbridges/testmod/Main.java
@@ -1,6 +1,10 @@
 package com.rbridges.testmod;
 
-@Mod()
+import com.rbridges.testmod.util.Reference;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class Main {
 
 }
--- src/main/java/com/rbridges/testmod/util/Reference.java
+++ src/main/java/com/rbridges/testmod/util/Reference.java
@@ -3,4 +3,9 @@ package com.rbridges.testmod.util;
 public class Reference {
 
     public static final String MOD_ID = "tm";
+    public static final String NAME = "Test Mod";
+    public static final String VERSION = "1.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "";
+    public static final String COMMON_PROXY_CLASS = "";
 }
